4f4022b726ad061867034d09819998b01a878341
Leonardo Thibes <eu@leonardothibes.com>
2014-03-20 13:51:45 -0300

    adicionando suporte a mais extensoes

--- manifests/compress.pp
+++ manifests/compress.pp
@@ -2,8 +2,20 @@ define util::compress($from = $title, $to)
 {
 	$ext = get_extension($to)
 	case $ext {
-		'zip': {
+		'zip','zp': {
 			util::compressor::zip::compress {$title:
+				from => $from,
+				to   => $to,
+			}
+		}
+		'gzip','gz','tgz': {
+			util::compressor::gzip::compress {$title:
+				from => $from,
+				to   => $to,
+			}
+		}
+		'bzip2','bz','bz2': {
+			util::compressor::gzip::compress {$title:
 				from => $from,
 				to   => $to,
 			}
